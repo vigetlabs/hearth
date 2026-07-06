8cb037041e3de4c0fa9d0f6e3d075ffea3c6b2c8
dio <er.dioneda@gmail.com>
2026-07-06 15:27:13 -0600

    Add package installation (#1)

---
+++ shell.nix
@@ -0,0 +1,24 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+pkgs.mkShell {
+  packages = with pkgs; [
+    ruby_4_0
+    rubyPackages.rails
+    tig
+
+    typescript
+    typescript-language-server
+
+    nodejs_24
+  ];
+
+  shellHook = ''
+    export GEM_HOME="$PWD/.gem"
+    export GEM_PATH="$GEM_HOME"
+    export PATH="$GEM_HOME/bin:$PATH"
+    echo "Entered Viget Project Shell..."
+    ruby -v
+    node --version
+    tsc --version
+  '';
+}
