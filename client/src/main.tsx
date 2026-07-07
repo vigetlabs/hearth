bae237aa8e99156085618526c8ec014c34b9ccdd
dio <er.dioneda@gmail.com>
2026-07-06 22:49:47 -0600

    Fix missing compiler option for config paths (#1)

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router";
-import { router } from "@/routes/router.ts";
+import { router } from "@/routes/router";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
