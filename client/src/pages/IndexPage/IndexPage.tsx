6319d25f914c30090d3e437972179677c3f2463f
dio <er.dioneda@gmail.com>
2026-07-08 21:16:12 -0600

    Add calendar page link (#42)

--- client/src/pages/IndexPage/IndexPage.tsx
+++ client/src/pages/IndexPage/IndexPage.tsx
@@ -15,8 +15,15 @@ export default function IndexPage() {
   return (
     <div>
       <div>API status: {data.status}</div>
-      <Link to="/users/signup">Signup</Link>
-      <Link to="/users/login">Login</Link>
+      <Link to="/users/signup" className="p-2">
+        Signup
+      </Link>
+      <Link to="/users/login" className="p-2">
+        Login
+      </Link>
+      <Link to="/calendar" className="p-2">
+        Calendar
+      </Link>
     </div>
   );
 }
