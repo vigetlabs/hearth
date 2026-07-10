9aac6da982f843284174d7de474642d93952cb4f
dio <er.dioneda@gmail.com>
2026-07-10 14:40:19 -0600

    Add updated types to frontend (n/a)

---
+++ client/src/types/api/offices.ts
@@ -0,0 +1,7 @@
+import type { components } from "@/types/api/openapi-schemas";
+
+export type Office = components["schemas"]["office"];
+
+export type OfficeResponse = components["schemas"]["office_response"];
+
+export type OfficesResponse = components["schemas"]["offices_response"];
