499a9203bdb5cf3f287c44fe3aa34096a9ffaf37
dio <er.dioneda@gmail.com>
2026-07-14 22:57:32 -0600

    Update openapi-schemas file (n/a)

--- client/src/types/api/visits.ts
+++ client/src/types/api/visits.ts
@@ -2,7 +2,7 @@ import type { components } from "@/types/api/openapi-schemas";
 
 export type Visit = components["schemas"]["visit"];
 
-export type CreateVisitRequest = components["schemas"]["create_visit_request"];
+export type CreateVisitRequest = components["schemas"]["create_visits_request"];
 
 export type VisitResponse = components["schemas"]["visit_response"];
 
