ddc66b67003ca43c24ecd64af83e3c99104bf594
dio <er.dioneda@gmail.com>
2026-07-15 23:23:54 -0600

    Add status column to visit model (#102)

--- api/app/models/visit.rb
+++ api/app/models/visit.rb
@@ -2,6 +2,11 @@ class Visit < ApplicationRecord
   belongs_to :office
   belongs_to :user
 
+  enum :status, {
+    planned: 0,
+    confirmed: 1
+  }
+
   validates :visit_date,
     presence: true,
     uniqueness: { scope: :user_id }
