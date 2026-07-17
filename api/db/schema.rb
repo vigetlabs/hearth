f4deecd274433c0c98fb5069602168a7bae4544e
dio <er.dioneda@gmail.com>
2026-07-17 14:30:29 -0600

    Change office id to be required in office presences model (#117)
    - Change office id to be required, as an office presence cannot be
    considered to be valid without an associated office

--- api/db/schema.rb
+++ api/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_07_17_173523) do
+ActiveRecord::Schema[8.1].define(version: 2026_07_17_202711) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -18,7 +18,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_07_17_173523) do
     t.string "connection_id", null: false
     t.datetime "created_at", null: false
     t.datetime "last_seen_at", null: false
-    t.bigint "office_id"
+    t.bigint "office_id", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
     t.index ["office_id"], name: "index_office_presences_on_office_id"
