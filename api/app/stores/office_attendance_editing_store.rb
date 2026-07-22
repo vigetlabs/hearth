6b86b6db6784c5f0b5abe8d03b244f2cb6f6d656
dio <er.dioneda@gmail.com>
2026-07-22 14:49:31 -0600

    Add office attendance editing store (#117)

---
+++ api/app/stores/office_attendance_editing_store.rb
@@ -0,0 +1,42 @@
+# typed: strict
+
+class OfficeAttendanceEditingStore
+  extend T::Sig
+
+  include TtlCalculator
+
+  STALE_AFTER = T.let(1.minutes, ActiveSupport::Duration)
+
+  sig { params(office_id: Integer, redis: Redis).void }
+  def initialize(office_id:, redis: REDIS)
+    @office_id = office_id
+    @redis = redis
+  end
+
+  sig { params(week_start: String, user_id: Integer).void }
+  def start_editing(week_start:, user_id:)
+    ApplicationRedis.with do |redis|
+      redis.zadd(
+        create_office_attendance_editing_key(week_start:),
+        expires_at(STALE_AFTER),
+        user_id
+      )
+    end
+  end
+
+  sig { params(week_start: String).returns(String) }
+  def create_office_attendance_editing_key(week_start:)
+    normalized_week_start = DateUtility.normalize_to_string(week_start)
+    raise ArgumentError, "Invalid data provided for Redis key" if normalized_week_start.nil?
+
+    [
+      ApplicationRedis::PREFIX,
+      "office-attending",
+      "office",
+      @office_id,
+      "week",
+      week_start,
+      "editing"
+    ].join(":")
+  end
+end
