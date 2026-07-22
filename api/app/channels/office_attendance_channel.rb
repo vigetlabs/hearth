64f7eff1b3836de280d14e8c96645ad6c0f81db6
dio <er.dioneda@gmail.com>
2026-07-22 16:38:27 -0600

    Add start editing function to attendance channel (#117)
    - This function will be called when a user clicks "edit week" after they
    click confirm

--- api/app/channels/office_attendance_channel.rb
+++ api/app/channels/office_attendance_channel.rb
@@ -16,6 +16,7 @@ class OfficeAttendanceChannel < ApplicationCable::Channel
     stream_for @office
   end
 
+  # Function only called in `confirm_week_service.rb`
   sig do
     params(
       office: Office,
@@ -35,6 +36,64 @@ class OfficeAttendanceChannel < ApplicationCable::Channel
     broadcast_to(
       office,
       payload
+    )
+  end
+
+  sig { params(data: ApplicationCable::Types::ChannelData).void }
+  def start_editing(data)
+    week_start = T.let(
+      DateUtility.normalized_week_start(data["week_start"]),
+      Date
+    )
+
+    week_dates = T.let(
+      DateUtility.week_dates(week_start),
+      T::Array[String]
+    )
+
+    confirmed_visits = T.let(
+      Visit
+        .where(
+          user: current_user,
+          office: @office,
+          visit_date: week_dates
+        )
+        .pluck(:visit_date)
+        .map(&:iso8601)
+        .to_set,
+      T::Set[String]
+    )
+
+    normalized_week_start = DateUtility.normalize_to_string(week_start)
+    raise ArgumentError, "Invalid week start" if normalized_week_start.nil?
+
+    office_attendance_editing_store.start_editing(
+      week_start: normalized_week_start,
+      user_id: current_user.id
+    )
+
+    week_dates.each do |date|
+      if confirmed_visits.include?(date)
+        planning_store.select(date:, user_id: current_user.id)
+      else
+        planning_store.deselect(date:, user_id: current_user.id)
+      end
+    end
+  end
+
+  sig { returns(OfficeAttendanceEditingStore) }
+  def office_attendance_editing_store
+    @office_attendance_editing_store ||= T.let(
+      OfficeAttendanceEditingStore.new(office_id: T.must(@office).id),
+      T.nilable(OfficeAttendanceEditingStore)
+    )
+  end
+
+  sig { returns(OfficePlanningStore) }
+  def planning_store
+    @planning_store ||= T.let(
+      OfficePlanningStore.new(office_id: T.must(@office).id),
+      T.nilable(OfficePlanningStore)
     )
   end
 end
