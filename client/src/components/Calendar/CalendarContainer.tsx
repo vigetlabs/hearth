6998354c133aeefff6f3b065f3eb07b61fb111c4
dio <er.dioneda@gmail.com>
2026-07-27 13:39:34 -0600

    Add calendar layout base styling (n/a)

--- client/src/components/Calendar/CalendarContainer.tsx
+++ client/src/components/Calendar/CalendarContainer.tsx
@@ -3,10 +3,9 @@ interface CalendarContainer {
 }
 
 export default function CalendarContainer({ children }: CalendarContainer) {
-   return (
-     <div className="flex min-h-0 flex-1 flex-col rounded-3xl border border-line bg-surface p-6 shadow-card">
-       {children}
-     </div>
-   );
- }
-
+  return (
+    <div className="flex min-h-0 flex-1 flex-col rounded-3xl border border-line bg-surface p-6 shadow-card">
+      {children}
+    </div>
+  );
+}
