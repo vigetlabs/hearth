48a9dd14ca659da6e30bb8caa6d44596d965f687
dio <er.dioneda@gmail.com>
2026-07-27 13:23:45 -0600

    Add calendar workspace imperative shell (n/a)

--- client/src/components/Calendar/CalendarWorkspace.tsx
+++ client/src/components/Calendar/CalendarWorkspace.tsx
@@ -1,3 +1,23 @@
+import { useCalendarData } from "@/hooks/useCalendarData";
+import { useCalendarScope } from "@/hooks/useCalendarScope";
+import CalendarPageSkeleton from "@/pages/CalendarPage/CalendarPageSkeleton";
+
 export default function CalendarWorkspace() {
-  return <div>Hello</div>;
+  const scope = useCalendarScope();
+
+  const data = useCalendarData({
+    userId: scope.user.id,
+    officeId: scope.activeOffice.id,
+    weekStartKey: scope.focusedWeekStartKey,
+  });
+
+  if (data.isPending) {
+    return <CalendarPageSkeleton />;
+  }
+
+  if (data.isError) {
+    return <div>Unable to load calendar data</div>;
+  }
+
+  return <div>State: {String(data.isError)}</div>;
 }
