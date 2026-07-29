53ec7516d9848288bb4cb64b8cbd53717e40d1c1
Ryan Dioneda <er.dioneda@gmail.com>
2026-07-29 13:29:07 -0600

    Add slack client functionality to create email to slack id map (#57)

--- api/app/clients/slack_client.rb
+++ api/app/clients/slack_client.rb
@@ -1,4 +1,6 @@
 class SlackClient
+  USERS_PAGE_SIZE = 200
+
   def initialize(token: Rails.application.credentials.dig(:slack, :bot_token))
     @client = Slack::Web::Client.new(token:)
   end
@@ -16,5 +18,40 @@ class SlackClient
     attributes[:blocks] = blocks if blocks.present?
 
     @client.chat.postMessage(**attributes)
+  end
+
+  def slack_ids_by_email
+    lookup = {}
+
+    workspace_users.each do |slack_user|
+      next if slack_user.deleted
+      next if slack_user.is_bot
+
+      email = slack_user.profile.email&.downcase
+      next if email.blank?
+
+      lookup[email] = slack_user.id
+    end
+
+    lookup
+  end
+
+  def workspace_users
+    members = []
+    cursor = nil
+
+    loop do
+      response = @client.users_list(
+        limit: USERS_PAGE_SIZE,
+        cursor:
+      )
+
+      members.concat(response.members)
+
+      cursor = response.response_metadata&.next_cursor
+      break if cursor.blank?
+    end
+
+    members
   end
 end
