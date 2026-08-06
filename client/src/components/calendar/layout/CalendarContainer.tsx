e652c8ab622852a9df8d34f734abf81ca2b3138d
Ryan Dioneda <er.dioneda@gmail.com>
2026-08-06 11:56:10 -0600

    Rewire calendar tour onboarding to calendar containe (n/a)

--- client/src/components/calendar/layout/CalendarContainer.tsx
+++ client/src/components/calendar/layout/CalendarContainer.tsx
@@ -1,11 +1,22 @@
+import { useCalendarScope } from "@/hooks/contexts/useCalendarScopeContext";
+import CalendarTour from "../extras/CalendarTour";
+
 interface CalendarContainer {
   children: React.ReactNode;
 }
 
 export default function CalendarContainer({ children }: CalendarContainer) {
+  const scope = useCalendarScope();
+  const isOnboardingComplete = scope.user.is_onboarding_complete;
+
   return (
     <div className="flex min-h-0 flex-1 flex-col rounded-3xl border border-line bg-surface p-6 shadow-card">
       {children}
+      {!isOnboardingComplete && (
+        <CalendarTour
+          firstName={scope.user.first_name}
+        />
+      )}
     </div>
   );
 }
